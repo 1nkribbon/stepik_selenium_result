b0b2f3ce99f69016dc866b21b2d167868cf3f03c
1nkribbon <seregio@rambler.ru>
2020-11-22 18:42:11 +0300

    add to cart

--- test_main_page.py
+++ test_main_page.py
@@ -17,8 +17,3 @@ def test_guest_should_see_login_link(browser):
 	page.open()
 	page.should_be_login_link()
  
-def test_guest_should_see_login_form(browser):
-	link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-	page = LoginPage(browser, link)
-	page.open()
-	page.should_be_login_page()
